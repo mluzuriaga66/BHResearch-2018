be47b4dea7c36e3275a0f389c03a4c53ad7a68f5
mluzuriaga66 <mluzuriaga66@tigermail.qcc.cuny.edu>
2018-06-14 14:56:36 -0400

    Finding Black Hole

---
+++ FindBlackHole.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import pynbody
+import matplotlib.pylab as plt
+plt.switch_backend("agg")
+
+#im loading the snapshot
+s = pynbody.load('/mnt/storm/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25cmb.4096g5HbwK1BH.004096')   
+
+
+# gonna start loading the halo'galaxies' now
+h = s.halos()
+
+#halo we need to look at is #2
+pynbody.analysis.angmom.faceon(h[2])
+
+s.physical_units()
+
+#^^ making all the data into a certain amount by mul everything for me
+
+
+#finding the black hole using function
+def findBH(s):
+    BHfilter = pynbody.filt.LowPass('tform',0.0)
+    BH = s.stars[BHfilter]
+    return BH
+    
+#finding what is the radius of the Black Hole at that time
+pynbody.analysis.halo.center(h[2],mode='hyb')
+
+#h2 is centered
+with pynbody.analysis.halo.center(h[2], mode='hyb'): 
+print(h[2]['pos'][0])
+print(h[2]['pos'][1])
+print(h[2]['pos'][2])
+
+cen_hyb = pynbody.analysis.halo.center(h[2],mode='hyb',retcen=True)
+print(cen_hyb)
