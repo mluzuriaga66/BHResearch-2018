136c65c62fd01ab47aeeaaeb46de6030b9c7ed64
mluzuriaga66 <39742003+mluzuriaga66@users.noreply.github.com>
2018-06-14 16:34:13 -0400

    organized the code

--- FindBlackHole.py
+++ FindBlackHole.py
@@ -11,6 +11,8 @@ plt.switch_backend("agg")
 #im loading the snapshot
 s = pynbody.load('/mnt/storm/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25cmb.4096g5HbwK1BH.004096')   
 
+#turns the numbers into useful units
+s.physical_units()
 
 #loading the halo aka galaxy
 h = s.halos()
@@ -18,8 +20,13 @@ h = s.halos()
 #halo/galaxy we need to look at is no.2
 pynbody.analysis.angmom.faceon(h[2])
 
-#turns the numbers into useful units
-s.physical_units()
+#this centers h2
+with pynbody.analysis.halo.center(h[2], mode='hyb'):
+    print(h[2]['pos'][0])
+    print(h[2]['pos'][1])
+    print(h[2]['pos'][2])
+    
+print ('pos')    
 
 #finding the black hole using function
 def findBH(s):
@@ -35,12 +42,6 @@ print ('BH')
 #finding what is the radius of the Black Hole at that time
 pynbody.analysis.halo.center(h[2],mode='hyb')
 
-#this centers h2
-with pynbody.analysis.halo.center(h[2], mode='hyb'):
-    print(h[2]['pos'][0])
-    print(h[2]['pos'][1])
-    print(h[2]['pos'][2])
-
-
-cen_hyb = pynbody.analysis.halo.center(h[2],mode='hyb',retcen=True)
-print(cen_hyb)
+#forgot what this is for
+#cen_hyb = pynbody.analysis.halo.center(h[2],mode='hyb',retcen=True)
+#print(cen_hyb)
