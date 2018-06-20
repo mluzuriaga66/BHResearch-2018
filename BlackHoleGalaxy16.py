a1ce3bb2481e18e6c8c60a1d730e2c1b1b57da95
mluzu <mluzu@winnipeg.internal.amnh.org>
2018-06-20 15:56:24 -0400

    Made a for loop

--- BlackHoleGalaxy16.py
+++ BlackHoleGalaxy16.py
@@ -12,66 +12,52 @@ s = pynbody.load('/mnt/storm/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25
 #turns the numbers into useful units
 s.physical_units()
 
-
 #loading the halo aka galaxy
 h = s.halos()
-h2=h[2]
 
-#halo/galaxy we need to look at is no. 2
-pynbody.analysis.angmom.faceon(h2)
-
-<<<<<<< HEAD
-#converting into useful units
-s.physical_units()
-=======
-#this centers h2
-pynbody.analysis.halo.center(h[2], mode='hyb')
-print(h[2]['pos'][0])
-print(h[2]['pos'][1])
-print(h[2]['pos'][2])
-
-print ('pos')
-
-#this centers h2
-pynbody.analysis.halo.center(h2, mode='hyb')
-
-#function to find the black holes
-def findBH(s):
+#function to find black hole
+def findBH(snap):
     BHfilter = pynbody.filt.LowPass('tform',0.0)
-    BH = s.stars[BHfilter]
+    BH = snap.stars[BHfilter]
     return BH
 
-#defining black hole
-BH = findBH(s)
+#function to find the halos that the galaxy is in
+def findBHhalos(snap):
+    BH = findBH(snap)
+    BHhalos = BH['amiga.grp']
+    return BHhalos
 
-#prints the black hole
-print BH
+#using the function the halos
+BHhalos = findBHhalos(s)
 
-#this is the position of black hole
-BHposition=BH['pos']
+#printing the halos
+print BHhalos
+
+for halo in BHhalos:
+    #halo/galaxy we need to look at is no. 2
+    pynbody.analysis.angmom.faceon(h[halo])
+    #defining black hole
+    BH = findBH(s)
+    #prints the black hole
+    print BH
+    #this is the position of black hole
+    BHposition=BH['pos']
+    #printing the position of black hole
+    print BHposition
+    #putting the x-values into a column
+    BHx= BHposition[:,0]
+    print BHx
+    #putting the y-values into a column
+    BHy= BHposition[:,1]
+    print BHy
+    #putting the z-values into a column
+    BHz= BHposition[:,2]
+    print BHz
+    #the .5 is the square root , this is the distance formula
+    distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
+    print distance
 
 
-#printing the position of black hole
-print BHposition
-
-#putting the x-values into a column
-BHx= BHposition[:,0]
-print BHx
-
-#putting the y-values into a column
-BHy= BHposition[:,1]
-print BHy
-
-#putting the z-values into a column
-BHz= BHposition[:,2]
-print BHz
-
-#distance function
-#def distance
-
-#the .5 is the square root , this is the distance formula
-distance =((BHx**2)+(BHy**2)+(BHz**2))**(.5)
-print distance
 
 
 
