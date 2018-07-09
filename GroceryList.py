623f7b2200439b6befdaa05c3693c12f53415ab4
mluzuriaga66 <mluzuriaga66@tigermail.qcc.cuny.edu>
2018-07-09 16:14:48 -0400

    made agrocery list

---
+++ GroceryList.py
@@ -0,0 +1,12 @@
+
+#declaring the variable to open the file
+f = open("grocerylist.txt", "w")
+#writing the file
+f.write("strawberry, blueberry, greek yogurt")
+
+#reading the grocery list file
+f = open("grocerylist.txt", "r")
+#printing the grocery list file
+print f.write()
+
+f.close()
