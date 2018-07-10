f81ea2811618f0ee266cbf90c7f5373d97af5277
mluzuriaga66 <mluzuriaga66@tigermail.qcc.cuny.edu>
2018-07-10 11:45:35 -0400

    added the appending and reading command, much more clear?

--- GroceryList.py
+++ GroceryList.py
@@ -1,12 +1,31 @@
 
 #declaring the variable to open the file
-f = open("grocerylist.txt", "w")
+#f = open("grocerylist.txt", "w")
 #writing the file
-f.write("strawberry, blueberry, greek yogurt")
+#f.write("strawberry, blueberry, greek yogurt")
 
 #reading the grocery list file
-f = open("grocerylist.txt", "r")
+#f = open("grocerylist.txt", "r")
 #printing the grocery list file
-print f.write()
+#print f.write()
+#f.close()
 
+#declaring the varaible "f" to open the file, and W is to wite in the file
+f = open("grocerylist.txt","w")
+#creating multiple linesat once, these are the what are in a yogurt parfait
+lines_of_text=["strawberry ","blueberry ","granola ","greek yogurt "]
+f.writelines(lines_of_text)
+#closing the file
 f.close()
+
+
+#opening the txt file and reading it
+f = open("grocerylist.txt","r")
+#printing the list
+print f.readlines()
+
+#appending the file
+f = open("grocerylist.txt","a")
+f.write("i still don't get this")
+f.close()
+
